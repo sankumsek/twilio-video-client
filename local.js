ab066f70be74aeefd8e07df0a8a566ecace7e16e
Sanjeev Sekar <sankumsek@gmail.com>
2016-11-01 16:24:18 -0700

    Generate and add temporary accessToken

--- local.js
+++ local.js
@@ -1,4 +1,4 @@
-var accessToken = 'GRAB THIS FROM YOUR TWILIO DASHBOARD';
+var accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzM1OWQ5NWIyNGUxNWM3MGY2ZjQ5ODFmYmM3ZGJhZDI4LTE0NzgwNDI2MDYiLCJpc3MiOiJTSzM1OWQ5NWIyNGUxNWM3MGY2ZjQ5ODFmYmM3ZGJhZDI4Iiwic3ViIjoiQUNkMDFjYWFmMDIyMDQxMzk3MWJlMmQ0YTUzOGZmMjQ5MyIsImV4cCI6MTQ3ODA0NjIwNiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoiVHJpYWwiLCJydGMiOnsiY29uZmlndXJhdGlvbl9wcm9maWxlX3NpZCI6IlZTN2U1ODFlYzEzMzk0M2FkYWE3NWU0OTk2MjQ3NDM5ZGEifX19.o07uKeTIlKVY5zhpP1KyT8qjnODA_c_y0fV-ZKszeRI';
 var accessManager = Twilio.AccessManager(accessToken);
 var client = Twilio.Conversations.Client(accessManager);
 
@@ -7,4 +7,7 @@ client.listen().then(function() {
     client.on('invite', function(invite) {
         invite.accept().then(onInviteAccepted);
     });
-});
+});var accessManager = Twilio.AccessManager(accessToken);
+var client = Twilio.Conversations.Client(accessManager);
+
+// Begin listening for invites to Twilio Video conversations.
